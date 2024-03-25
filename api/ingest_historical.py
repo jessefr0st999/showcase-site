c885c54c136d5735abd55b4e595b5fbbc4339a6c
Jesse <jessefrost999@gmail.com>
2024-03-25 21:49:57 +1100

    bulk upsert for ingest scripts

--- api/ingest_historical.py
+++ api/ingest_historical.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, desc, and_, or_, text
 from sqlalchemy.orm import Session
+from sqlalchemy.dialects.postgresql import insert
 from xml.etree import ElementTree
 from lxml.etree import XMLParser
 import requests
@@ -18,12 +19,12 @@ SEASON_MATCH_IDS = {
     2016: (874, 1107),
     2017: (1135, 1341),
     2018: (1360, 1566),
-    # 2019: (1585, 1791),
-    # 2020: (1810, 2188),
-    # 2021: (2198, 2412),
-    # 2022: (2422, 2628),
-    # 2023: (2638, 2853),
-    # 2024: (2863, 2878),
+    2019: (1585, 1791),
+    2020: (1810, 2188),
+    2021: (2198, 2412),
+    2022: (2422, 2628),
+    2023: (2638, 2853),
+    2024: (2863, 2878),
 }
 MATCH_PROPERTIES_MAP = {
     'Round': 'round',
@@ -168,27 +169,38 @@ def calculate_ladder(season=None, latest_round=True):
 
 if __name__ == '__main__':
     for season in SEASON_MATCH_IDS:
-        match_id, end_id = SEASON_MATCH_IDS[season]
+        start_id, end_id = SEASON_MATCH_IDS[season]
+        match_id = start_id
+        match_stats = []
+        player_stats = []
+        player_season_stats = []
         while match_id <= end_id:
             try:
-                match_stats, player_stats, player_season_stats = \
+                _match_stats, _player_stats, _player_season_stats = \
                     get_match_data(match_id)
-                # Consider using insert from sqlalchemy.dialects.postgresql:
-                # stmt = insert(Table).values(values).on_conflict_do_update(...)
-                # session.execute(stmt)
-                with Session(engine) as session:
-                    session.merge(Matches(**match_stats))
-                    for x in player_season_stats:
-                        session.merge(PlayersBySeason(**x))
-                    session.commit()
-                    for x in player_stats:
-                        session.merge(PlayerStats(**x))
-                    session.commit()
-                print(f'Values upserted for match with ID {match_id}')
+                if match_id % 10 == 0:
+                    print(f'{season}: Data for match with ID {match_id} obtained'
+                        f' ({match_id - start_id} / {end_id - start_id})')
+                match_stats.append(_match_stats)
+                player_stats.extend(_player_stats)
+                player_season_stats.extend(_player_season_stats)
             except requests.HTTPError:
                 print(f'No match found with ID {match_id}')
             match_id += 1
-        calculate_ladder(season, latest_round=False)
+        with Session(engine) as session:
+            query = insert(Matches).values(match_stats)\
+                .on_conflict_do_nothing()
+            session.execute(query)
+            query = insert(PlayersBySeason).values(player_season_stats)\
+                .on_conflict_do_nothing()
+            session.execute(query)
+            session.commit()
+            query = insert(PlayerStats).values(player_stats)\
+                .on_conflict_do_nothing()
+            session.execute(query)
+            session.commit()
+        print(f'2023: Values upserted for matches with IDs {start_id} to {end_id}')
+        calculate_ladder(season, latest_round=True)
     with Session(engine) as session:
         sql = text('REFRESH MATERIALIZED VIEW player_season_stats;')
         session.execute(sql)
--- api/ingest_live.py
+++ api/ingest_live.py
@@ -2,13 +2,14 @@ import schedule
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, desc, text
 from sqlalchemy.orm import Session
+from sqlalchemy.dialects.postgresql import insert
 from datetime import datetime
 import time
 import os
 import requests
 
 from .schema import *
-from ingest_historical import get_match_data, calculate_ladder
+from .ingest_historical import get_match_data, calculate_ladder
 
 load_dotenv()
 engine = create_engine(os.getenv('DATABASE_URI'))
@@ -25,25 +26,38 @@ def job():
             .limit(3).all()
         last_match_ids = [x[0] for x in last_match_ids][::-1]
     future_match_ids = [last_match_ids[-1] + i + 1 for i in range(4)]
+    match_stats = []
+    player_stats = []
+    player_season_stats = []
     for match_id in [*last_match_ids, *future_match_ids]:
         try:
-            match_stats, player_stats, player_season_stats = \
+            _match_stats, _player_stats, _player_season_stats = \
                 get_match_data(match_id)
-            with Session(engine) as session:
-                session.merge(Matches(**match_stats))
-                for x in player_season_stats:
-                    session.merge(PlayersBySeason(**x))
-                session.commit()
-                for x in player_stats:
-                    session.merge(PlayerStats(**x))
-                session.commit()
-                print(f'Values upserted for match with ID {match_id}')
+            print(f'Data for match with ID {match_id} obtained')
+            match_stats.append(_match_stats)
+            player_stats.extend(_player_stats)
+            player_season_stats.extend(_player_season_stats)
         except requests.HTTPError:
             print(f'No match found with ID {match_id}')
+    with Session(engine) as session:
+        query = insert(Matches).values(match_stats)\
+            .on_conflict_do_nothing()
+        session.execute(query)
+        query = insert(PlayersBySeason).values(player_season_stats)\
+            .on_conflict_do_nothing()
+        session.execute(query)
+        session.commit()
+        query = insert(PlayerStats).values(player_stats)\
+            .on_conflict_do_nothing()
+        session.execute(query)
+        session.commit()
+    print(f'2023: Values upserted for matches with IDs {last_match_ids[0]}'
+            f' to {future_match_ids[-1]}')
     calculate_ladder()
     with Session(engine) as session:
         sql = text('REFRESH MATERIALIZED VIEW player_season_stats;')
         session.execute(sql)
+        session.commit()
 
 if __name__ == '__main__':
     schedule.every(5).minutes.do(job)
