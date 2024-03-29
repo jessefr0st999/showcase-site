9207bd0d66f05bd412f0adbebcefd234a1e68e66
Jesse <jessefrost999@gmail.com>
2024-03-29 17:00:23 +1100

    logging for websocket server

--- websocket/server.py
+++ websocket/server.py
@@ -2,6 +2,7 @@ import asyncio
 from tornado.web import Application, RequestHandler
 from tornado.websocket import WebSocketHandler
 from tornado.escape import json_decode
+import logging
 
 ALLOWED_WS_ORIGINS = [
     'http://127.0.0.1:5000',
@@ -22,11 +23,13 @@ ALLOWED_HTTP_REMOTE_IPS = [
     '54.254.162.138',
 ]
 
+logger = logging.getLogger(__name__)
+
 CLIENTS = set()
 class SocketHandler(WebSocketHandler):
     @classmethod
     def send_message(cls, message: str):
-        print(f'Sending message to clients {CLIENTS} ')
+        logger.info(f'Sending message to clients {CLIENTS}')
         for client in CLIENTS:
             client.write_message(message)
     
@@ -34,15 +37,15 @@ class SocketHandler(WebSocketHandler):
         pass
     
     def open(self):
+        logger.info(f'Opening connection with client {self}')
         CLIENTS.add(self)
-        self.write_message('connected')
-        print('open')
+        self.write_message('Connected')
     
     def on_message(self, message):
-        print('message received:', message)
+        logger.info(f'Message received from client {self}: {message}')
     
     def on_close(self):
-        print('on_close')
+        logger.info(f'Closing connection with client {self}')
         CLIENTS.remove(self)
     
     def check_origin(self, origin):
@@ -50,9 +53,13 @@ class SocketHandler(WebSocketHandler):
     
 class HttpHandler(RequestHandler):
     def get(self):
+        logger.info(f'GET request received from IP {self.request.remote_ip},'
+            f' hostname {self.request.host_name}')
         self.write('200 OK')
         
     def post(self):
+        logger.info(f'POST request received from IP {self.request.remote_ip},'
+            f' hostname {self.request.host_name}')
         if self.request.host_name not in ALLOWED_HTTP_HOST_NAMES and \
                 self.request.remote_ip not in ALLOWED_HTTP_REMOTE_IPS:
             self.write('403 Forbidden')
@@ -62,6 +69,8 @@ class HttpHandler(RequestHandler):
         self.write('200 OK')
 
 async def main():
+    logging.basicConfig(level=logging.INFO)
+    logger.info('Server starting')
     app = Application([
         (r'/', HttpHandler),
         (r'/ws', SocketHandler),
